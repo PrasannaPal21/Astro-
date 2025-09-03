a5b717278a2d91965890a55ef4e8cbe26325a4a8
Prasanna Pal <prasannapal273@gmail.com>
2025-09-03 16:49:10 +0530

    Delete Astroyamer directory

--- Astroyamer/src/data/worldCities.js
+++
@@ -1,7 +0,0 @@
-export const cities = [
-  { value: 'new-york', label: 'New York, USA' },
-  { value: 'london', label: 'London, UK' },
-  { value: 'tokyo', label: 'Tokyo, Japan' },
-  // Add more cities as needed
-];
-
